cc7d3da34c4c210be7033a03c92ccb2c10ba7b40
Edric-Matwiejew <edric.matviev@gmail.com>
2025-10-10 16:48:05 +0800

    install hip

--- scripts/install_software_stack.sh
+++ scripts/install_software_stack.sh
@@ -10,6 +10,7 @@ source settings.sh
 bash scripts/install_gcc.sh
 bash scripts/install_perl.sh
 bash scripts/install_gcc_development_environment.sh
+bash scripts/install_prgenv.sh
 bash scripts/install_python.sh
 bash scripts/install_cmake.sh
 bash scripts/install_ninja.sh
