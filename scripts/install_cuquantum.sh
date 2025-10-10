7d63305a6f14bd93f48ba8883c1c2957bded731e
Edric-Matwiejew <edric.matviev@gmail.com>
2025-10-10 16:54:13 +0800

    move versions to settings.sh, update download paths

--- scripts/install_cuquantum.sh
+++ scripts/install_cuquantum.sh
@@ -1,55 +1,66 @@
+#!/bin/bash
+
 source settings.sh
 
-# https://docs.nvidia.com/cuda/cuquantum/latest/getting-started/index.html
 CUQUANTUM_ARCH=sbsa
-CUQUANTUM_VERSION=24.03.0
-CUTENSOR_VERSION=2.0.1
-CUQUANTUM_BUILD_NUMBER=4
-NVHPC_OPENMPI_VERSION=3.1.5
 
 module load hpcx-mt-ompi
 module load nvhpc/$NVHPC_VERSION
-module load cutensor/$CUTENSOR_VERSION
-module load gcc
+module load gcc/$GCC_VERSION
 
 CUDA_MAJOR_VERSION=$(nvcc --version | grep -o "release [0-9]\+\.[0-9]\+" | awk '{split($2, a, "."); print a[1]}')
 
-CUQUANTUM_INSTALL_PREFIX=$INSTALL_PREFIX/cuquantum-${CUQUANTUM_VERSION}
-CUQUANTUM_ARCHIVE=cuquantum-linux-${CUQUANTUM_ARCH}-${CUQUANTUM_VERSION}.${CUQUANTUM_BUILD_NUMBER}_cuda${CUDA_MAJOR_VERSION}-archive
-CUQUANTUM_MODULE_PREFIX=$MODULE_PREFIX/cuquantum
-
-
 cd $BUILD_PREFIX
 
-# get archive and extract
-wget https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-${CUQUANTUM_ARCH}/$CUQUANTUM_ARCHIVE.tar.xz
-tar -xvf $CUQUANTUM_ARCHIVE.tar.xz
+for i in "${!CUQUANTUM_VERSIONS[@]}"; do
+    CUQUANTUM_VERSION=${CUQUANTUM_VERSIONS[$i]}
+    CUQUANTUM_BUILD=${CUQUANTUM_BUILD_NUMBER[$i]}
+    CUQUANTUM_INSTALL_PREFIX=$INSTALL_PREFIX/cuquantum-${CUQUANTUM_VERSION}
+    CUQUANTUM_MODULE_PATH=$MODULE_PREFIX/cuquantum/$CUQUANTUM_VERSION.lua
+    CUQUANTUM_ARCHIVE=cuquantum-linux-${CUQUANTUM_ARCH}-${CUQUANTUM_VERSION}.${CUQUANTUM_BUILD}-archive
 
-# MPI_HOME variable set by hpcx-mt-ompi
-cd $CUQUANTUM_ARCHIVE/distributed_interfaces
-$(which gcc) -shared -std=c99 -fPIC \
-	-I${NVHPC_ROOT}/cuda/include -I../include -I${MPI_HOME}/include \
- 	 cutensornet_distributed_interface_mpi.c \
-  	-L${MPI_HOME}/lib -lmpi \
-  	-o libcutensornet_distributed_interface_mpi.so
-cd $BUILD_PREFIX
+    module load cutensor/$CUTENSOR_VERSION
+    
+    # Check if already installed
+    if [[ -d "$CUQUANTUM_INSTALL_PREFIX" ]]; then
+        echo "cuQuantum $CUQUANTUM_VERSION (Build $CUQUANTUM_BUILD) already installed. Skipping."
+        continue
+    fi
+    
+    echo "Installing cuQuantum $CUQUANTUM_VERSION (Build $CUQUANTUM_BUILD)"
+    wget https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-${CUQUANTUM_ARCH}/$CUQUANTUM_ARCHIVE.tar.xz
+    tar -xvf $CUQUANTUM_ARCHIVE.tar.xz
+    
+    # Build MPI shared library
+    cd $CUQUANTUM_ARCHIVE/distributed_interfaces
+    $(which gcc) -shared -std=c99 -fPIC \
+        -I${NVHPC_ROOT}/cuda/include -I../include -I${MPI_HOME}/include \
+        cutensornet_distributed_interface_mpi.c \
+        -L${MPI_HOME}/lib -lmpi \
+        -o libcutensornet_distributed_interface_mpi.so
+	    
+    cd $BUILD_PREFIX
+    
+    # Install cuQuantum
+    mkdir -p $CUQUANTUM_INSTALL_PREFIX
+    mv $CUQUANTUM_ARCHIVE/* $CUQUANTUM_INSTALL_PREFIX/.
+    rm -rf $CUQUANTUM_ARCHIVE.tar.xz
+    
+    # Setup module file
+    MODULE_TEMP_PATH="$BUILD_PREFIX/$CUQUANTUM_VERSION.lua"
+    cp $SETUP_PREFIX/modules/cuquantum_module $MODULE_TEMP_PATH
+    sed -i "s|CUQUANTUMVERSION|$CUQUANTUM_VERSION|g" "$MODULE_TEMP_PATH"
+    sed -i "s|GCCVERSION|$GCC_VERSION|g" "$MODULE_TEMP_PATH"
+    sed -i "s|CUDAVERSION|$CUDA_MAJOR_VERSION|g" "$MODULE_TEMP_PATH"
+    sed -i "s|CUTENSORVERSION|$CUTENSOR_VERSION|g" "$MODULE_TEMP_PATH"
+    sed -i "s|NVHPCVERSION|$NVHPC_VERSION|g" "$MODULE_TEMP_PATH"
+    sed -i "s|CUQUANTUMROOT|$CUQUANTUM_INSTALL_PREFIX|g" "$MODULE_TEMP_PATH"
+    mkdir -p $MODULE_PREFIX/cuquantum
+    mv $MODULE_TEMP_PATH $CUQUANTUM_MODULE_PATH
 
-# install
-mkdir -p $CUQUANTUM_INSTALL_PREFIX
-mv $CUQUANTUM_ARCHIVE/* $CUQUANTUM_INSTALL_PREFIX/.
-rm -rf $CUQUANTUM_ARCHIVE.tar.xz
+done
 
-MODULE_TEMP_PATH="$BUILD_PREFIX/$CUQUANTUM_VERSION.lua"
-cp $SETUP_PREFIX/modules/cuquantum_module $MODULE_TEMP_PATH
-sed -i "s|CUQUANTUMVERSION|$CUQUANTUM_VERSION|g" "$MODULE_TEMP_PATH"
-sed -i "s|CUDAVERSION|$CUDA_MAJOR_VERSION|g" "$MODULE_TEMP_PATH"
-sed -i "s|NVHPCVERSION|$NVHPC_VERSION|g" "$MODULE_TEMP_PATH"
-sed -i "s|CUQUANTUMROOT|$CUQUANTUM_INSTALL_PREFIX|g" "$MODULE_TEMP_PATH"
-mkdir -p $CUQUANTUM_MODULE_PREFIX
-mv $MODULE_TEMP_PATH $CUQUANTUM_MODULE_PREFIX/.
-
-module load gcc
-module load cutensor/$CUTENSOR_VERSION
-module load hpcx-mt-ompi
-module load nvhpc/$NVHPC_VERSION
+module unload cutensor/$CUTENSOR_VERSION
+module unload hpcx-mt-ompi
+module unload nvhpc/$NVHPC_VERSION
 
